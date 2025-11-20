3ba8f6feb8b9b9b75f67be7f33e4062793966799
Everett De Leon <the00110100lion@gmail.com>
2025-11-20 05:02:05 -0800

    change port and add cors

--- server/index.js
+++ server/index.js
@@ -31,7 +31,14 @@ try {
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false, limit: '10mb' }));
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3012",
+    "https://beta.borrowarr.com",
+    "http://beta.borrowarr.com"
+  ],
+  credentials: true
+}));
 
 // Ensure all models are loaded BEFORE database sync
 require("./models/User");
